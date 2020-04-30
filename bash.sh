b84ccaaef789640b0319db951451f190f2810c5b
kettle <kettleofrassel@gmail.com>
2020-04-29 23:00:21 -0700

    expressions

--- bash.sh
+++ bash.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo Hello, World!!!
+# let <arithmetic expression>
+
+let a=7*7
+echo $a
+
+# expr <expression>
+expr 8 \* 8
+expr 5 + 5
+
+b=$((4+4))
+echo $b
+
+h='Hello, World!'
+echo 'length of h' ${#h}
