0c1f54a938892442a6cf6f17ced798bc93d77bd5
kettle <kettleofrassel@gmail.com>
2020-04-29 23:17:52 -0700

    if statement

--- bash.sh
+++ bash.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo Hello, World!!!
+if [ $1 -gt 100 ]
+then
+  echo 'That is large number'
+else
+  echo 'That is smaller than 100'
+fi
+
+if [ $1 -eq 42 ]
+then
+  echo 'Ohhh, yeah'
+fi
+
+cal
