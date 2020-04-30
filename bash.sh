e8f1a2c579b7d61c60053a6ecf081c15b1f89106
kettle <kettleofrassel@gmail.com>
2020-04-29 23:25:10 -0700

    loops

--- bash.sh
+++ bash.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo Hello, World!!!
+counter=1
+while [ $counter -le 10 ]
+do
+  echo $counter
+  ((counter++))
+done
+
+names='Stan Kyle Cartman Butters'
+
+for name in $names
+do
+  echo $name
+done
+
+echo 'All done'
