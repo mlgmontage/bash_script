204271d5344436e2c14b8b71ffda6470f9c7646e
kettle <kettleofrassel@gmail.com>
2020-04-29 23:30:46 -0700

    calc

--- bash.sh
+++ bash.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo Hello, World!!!
+echo 'Welcome to console calculator'
+
+echo 'Enter first operand'
+read first
+
+echo 'Enter second operand'
+read second
+
+echo 'Enter operation'
+read op
+
+if [ op -eq '+' ]
+then
+  echo $(($first+$second))
+fi
