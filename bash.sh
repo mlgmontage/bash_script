2b413971374fc52ade9c29712a988977bdc5c5fe
kettle <kettleofrassel@gmail.com>
2020-04-29 22:50:27 -0700

    variable

--- bash.sh
+++ bash.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Hello, World!!!
+my_variable=Hello
+another_variable=World
+
+echo $my_variable
+
+echo "this is first argument " $1
+echo "How many arguments were passsed" $#
+echo "Random number" $RANDOM
+echo "Hostname" $HOSTNAME
